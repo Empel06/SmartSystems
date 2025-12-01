6703871815606ee4c30a71da5371a75d2dff4eaf
5CD322B26V\12302093 <12302093@student.pxl.be>
2025-12-01 11:21:12 +0100

    Changed commands => werken met offline trainingmodel dus engels is makkelijker

--- src/commands.py
+++ src/commands.py
@@ -2,8 +2,8 @@
 COMMANDS = {
     "start_timer": ["start"],
     "pause_timer": ["pause"],
-    "work_time": ["tijd"],
-    "room_temp": ["temperatuur"],
+    "work_time": ["time"],
+    "room_temp": ["temperature"],
     "pomodoro": ["pomodoro"]
 }
 LABELS = list(COMMANDS.keys())
