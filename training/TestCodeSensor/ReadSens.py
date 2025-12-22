190210407aac58a7e92dadce2f6a5465ec438eb6
5CD322B26V\12302093 <12302093@student.pxl.be>
2025-12-22 16:35:36 +0100

    Fix ENS160 air quality fields in ReadSens.py

--- training/TestCodeSensor/ReadSens.py
+++ training/TestCodeSensor/ReadSens.py
@@ -29,7 +29,7 @@ while True:
     temp, rh = update_environment()
 
     # ENS160-waarden uitlezen
-    aq = ens.air_quality              # IAQ index (0–3 of 1–5 afhankelijk van lib)
+    aq = ens.aqi              # IAQ index (0–3 of 1–5 afhankelijk van lib)
     tvoc = ens.TVOC                  # ppb
     eco2 = ens.eCO2                  # ppm
 
